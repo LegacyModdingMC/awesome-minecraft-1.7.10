585b7bc9f1ac8e986c51d26f4b91553218e2ac85
makamys <makamys@outlook.com>
2020-03-06 16:52:23 +0100

    Add mixins

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,9 +1,20 @@
 package com.example.examplemod;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
+import net.minecraft.launchwrapper.Launch;
+import net.minecraftforge.common.MinecraftForge;
+
+import com.example.examplemod.mixin.IEntity;
+
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent.ClientTickEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
@@ -14,7 +25,17 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+        FMLCommonHandler.instance().bus().register(this);
+    }
+    
+    @SubscribeEvent
+    public void onClientTick(ClientTickEvent event) {
+        Entity player = Minecraft.getMinecraft().thePlayer;
+        
+        if(player != null){
+            if(((IEntity)player).isInWeb()) { // using accessor to access protected field 
+                System.out.println("(MIXIN EXAMPLE) You're in a web!");
+            }
+        }
     }
 }
